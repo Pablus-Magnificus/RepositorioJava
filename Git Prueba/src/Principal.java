9720507d257fd4c85fbd79935b8d2a38f000058f
admin <admin@AULA05-PC21>
2019-02-15 13:43:43 +0100

    Primera version tabla mult 5

---
+++ Git Prueba/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero = 5;
+		for (int i=1; i<=10; i++) {
+			System.out.println
+(numero + " x " + i + " = " + (numero*i));
+		}
+
+	}
+
+}
